05dba564ce7218936c4b38f473142a66fc5890d7
Adhish Bahl <adhishbahl0@gmail.com>
2021-08-01 21:10:04 +0530

    selected elements

---
+++ app.js
@@ -0,0 +1,6 @@
+const oldPrice = document.querySelector("#oldPrice");
+const stockNumber = document.querySelector("#stockNumber");
+const newPrice = document.querySelector("#newPrice");
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
+
