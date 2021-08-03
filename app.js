6ed63a83e14fd255227048e57faf5979dd6400ed
Adhish Bahl <adhishbahl0@gmail.com>
2021-08-03 23:47:21 +0530

    changing background color according to result

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const stockNumberDOM = document.querySelector("#stockNumber");
 const newPriceDOM = document.querySelector("#newPrice");
 const btn = document.querySelector(".btn");
 const output = document.querySelector(".output");
+const container = document.querySelector(".container");
+const input = document.querySelectorAll("input");
 output.style.display = "none";  
 
 btn.addEventListener("click", ()=> {
@@ -16,7 +18,19 @@ btn.addEventListener("click", ()=> {
         let percentage = (finalPrice / invest) * 100;
         percentage = percentage.toFixed(2);
         let profit = finalPrice;
-        profit = profit.toFixed(2)
+        profit = profit.toFixed(2);
+        if(percentage >= 50) {
+            container.style.backgroundColor = "rgb(22, 189, 78)";
+            input[0].style.backgroundColor = "rgb(22, 189, 78)";
+            input[1].style.backgroundColor = "rgb(22, 189, 78)";
+            input[2].style.backgroundColor = "rgb(22, 189, 78)";
+        }
+        else{
+            container.style.backgroundColor = "rgb(167, 94, 42)";
+            input[0].style.backgroundColor = "rgb(167, 94, 42)";
+            input[1].style.backgroundColor = "rgb(167, 94, 42)";
+            input[2].style.backgroundColor = "rgb(167, 94, 42)";
+        }
         output.textContent = `You had gain ${percentage}% of your investment, that is profit of ₹${profit}`;
     } else if(finalPrice == 0) {
         output.textContent = "You neither had a Profit nor a Loss";
@@ -25,6 +39,18 @@ btn.addEventListener("click", ()=> {
         percentage = percentage.toFixed(2);
         let loss = invest - outputPrice;
         loss = loss.toFixed(2);
+        if(percentage >= 50) {
+            container.style.backgroundColor = "rgb(165, 31, 31)";
+            input[0].style.backgroundColor = "rgb(165, 31, 31)";
+            input[1].style.backgroundColor = "rgb(165, 31, 31)";
+            input[2].style.backgroundColor = "rgb(165, 31, 31)";
+        }
+        else{
+            container.style.backgroundColor = "rgb(167, 94, 42)";
+            input[0].style.backgroundColor = "rgb(167, 94, 42)";
+            input[1].style.backgroundColor = "rgb(167, 94, 42)";
+            input[2].style.backgroundColor = "rgb(167, 94, 42)";
+        }
         output.textContent = `You had lost ${percentage}% of your investment, that is loss of ₹${loss}`;
     }
     output.style.display = "block";
